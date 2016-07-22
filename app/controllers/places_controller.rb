3022ef65097e412eb019d6bc44f5ba1e93daa2ce
Robert Luckfield <rluck0419@gmail.com>
2016-07-21 21:04:32 -0500

    user controller crud

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,58 @@
 class PlacesController < ApplicationController
+  def index
+    render locals: { students: Student.all }
+  end
+  
+  def show
+    if Student.exists?(params[:id])
+      render locals: { student: Student.find(params[:id]) }
+    else
+      render html: 'Student not found', status: 404
+    end
+  end
+
+  def new
+    render locals: {
+      student: Student.new
+    }
+  end
+
+  def create
+    student = Student.new(student_params)
+    if student.save
+      redirect_to student
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render locals: {
+      student: Student.find(params[:id])
+    }
+  end
+
+  def update
+    student = Student.find(params[:id])
+    if student.update(student_params)
+      redirect_to student
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if Student.exists?(params[:id])
+      Student.destroy(params[:id])
+      flash[:notice] = "Student deleted"
+      redirect_to students
+    else
+      flash[:alert] = "There was an error - please try again"
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :teacher_id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,71 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    if User.exists?(params[:id])
+      render json: User.find(params[:id]), status: 200
+    else
+      render json: { message: "User not found." }, status: 404
+    end
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
+    if current_user != @user
+      flash[:alert] = "You cannot edit other users."
+      redirect_to users_path
+    end
   end
 
   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    if @user.id == current_user.id
+      @user.destroy
+    end
+
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password)
   end
 end
