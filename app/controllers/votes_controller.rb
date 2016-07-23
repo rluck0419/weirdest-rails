2d6e868514cf264f2e450dc73fe3a3d11520c6b8
Robert Luckfield <rluck0419@gmail.com>
2016-07-23 14:44:44 -0500

    crud for votes - voting should be working now too

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -12,14 +12,51 @@ class VotesController < ApplicationController
   end
 
   def create
-    authenticate_token?(params[:auth_token])
+    if authenticate_token?(params[:auth_token])
+      vote = Vote.new(vote_params)
+      if vote.save
+        render json: { vote: vote }
+      else
+        render json: { status: 404 }
+      end
+    else
+      render json: { message: "You must be logged in to do that", status: 401 }
+    end
   end
 
   def update
-    authenticate_token?(params[:auth_token])
+    vote = Vote.find(params[:id])
+    if vote
+      if authenticate_ownership?(params[:auth_token], vote)
+        if vote.update(vote_params)
+          render json: { vote: vote }
+        else
+          render json: { status: 404 }
+        end
+      else
+        render json: { message: "You must be logged in to do that", status: 401 }
+      end
+    else
+      render json: { message: "Vote does not exist", status: 401 }
+    end
   end
 
   def destroy
-    authenticate_token?(params[:auth_token])
+    vote = Vote.find(params[:id])
+    if vote
+      if authenticate_ownership?(params[:auth_token], vote)
+        Vote.destroy(params[:id])
+        render json: { message: "Vote was successfully deleted" }
+      else
+        render json: { message: "You must have created this vote to delete it", status: 404 }
+      end
+    else
+      render json: { message: "Vote does not exist", status: 401 }
+    end
+  end
+
+  private
+  def vote_params
+    params.require(:vote).permit(:place_id, :user_id)
   end
 end
